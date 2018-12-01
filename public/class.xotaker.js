5623f463172df215b87aa97bf1480174fb319cdb
Your Name <you@example.com>
2018-12-01 13:55:35 +0400

    poxeci kerparnery

--- public/class.xotaker.js
+++ public/class.xotaker.js
@@ -1,63 +1,56 @@
-class Xotakerik{
-    constructor(x,y){
-        this.x = x;
-        this.y = y;
+class Xotakerik extends LivingCreature {
+    constructor(x, y, index) {
+        super(x, y, index);
         this.en = 8;
         this.gorcakic = 0;
     }
-    cord(){
+    cord() {
         this.cords = [
-            [this.x - 1,this.y - 1],
-            [this.x    ,this.y - 1],
-            [this.x + 1,this.y - 1],
-            [this.x - 1,this.y    ],
-            [this.x + 1,this.y    ],
-            [this.x - 1,this.y + 1],
-            [this.x    ,this.y + 1],
-            [this.x + 1,this.y + 1]
+            [this.x - 1, this.y - 1],
+            [this.x, this.y - 1],
+            [this.x + 1, this.y - 1],
+            [this.x - 1, this.y],
+            [this.x + 1, this.y],
+            [this.x - 1, this.y + 1],
+            [this.x, this.y + 1],
+            [this.x + 1, this.y + 1]
         ];
         return this.cords;
     }
-    cordTrue(t){
+    cordTrue(t) {
         var ret = this.cord();
         var trueCord = [];
-        for(var i = 0;i<ret.length;i++)
-        {
-            if(ret[i][0] <= (matrix[0].length - 1) && ret[i][0] >= 0 && ret[i][1] <= (matrix.length - 1) && ret[i][1] >= 0)
-            {
-                if(matrix[ret[i][1]][ret[i][0]] == t)
-                {
+        for (var i = 0; i < ret.length; i++) {
+            if (ret[i][0] <= (matrix[0].length - 1) && ret[i][0] >= 0 && ret[i][1] <= (matrix.length - 1) && ret[i][1] >= 0) {
+                if (matrix[ret[i][1]][ret[i][0]] == t) {
                     trueCord.push(ret[i]);
                 }
             }
         }
         return trueCord;
     }
-    mahanal(){
-        for(var i = 0;i<xotakerner.length;i++)
-        {
-            if(xotakerner[i].x == this.x && xotakerner[i].y == this.y){
+    mahanal() {
+        for (var i = 0; i < xotakerner.length; i++) {
+            if (xotakerner[i].x == this.x && xotakerner[i].y == this.y) {
                 matrix[this.y][this.x] = 0;
                 xotakerner.splice(i, 1);
             }
         }
     }
-    bazmanal(){
+    bazmanal() {
         var arrcord = random(this.cordTrue(0));
-        if(arrcord){
-            xotakerner.push(new Xotakerik(arrcord[0],arrcord[1]));
+        if (arrcord) {
+            xotakerner.push(new Xotakerik(arrcord[0], arrcord[1]));
             matrix[arrcord[1]][arrcord[0]] = 2;
             this.gorcakic = 0;
         }
     }
-    move(){
-        if(random(this.cordTrue(1)))
-        {
+    move() {
+        if (random(this.cordTrue(1))) {
             var arrcord = random(this.cordTrue(1));
             matrix[arrcord[1]][arrcord[0]] = 2;
-            for(var i = 0;i<xoter.length;i++)
-            {
-                if(xoter[i].x == arrcord[0] && xoter[i].y == arrcord[1]){
+            for (var i = 0; i < xoter.length; i++) {
+                if (xoter[i].x == arrcord[0] && xoter[i].y == arrcord[1]) {
                     xoter.splice(i, 1);
                 }
             }
@@ -67,7 +60,7 @@ class Xotakerik{
             this.en = 8;
             this.gorcakic++;
         }
-        else if(random(this.cordTrue(0))){
+        else if (random(this.cordTrue(0))) {
             var arrcord = random(this.cordTrue(0));
             matrix[arrcord[1]][arrcord[0]] = 2;
             matrix[this.y][this.x] = 0;
@@ -75,10 +68,10 @@ class Xotakerik{
             this.y = arrcord[1];
             this.en--;
         }
-        if(this.en <= 0){
+        if (this.en <= 0) {
             this.mahanal();
         }
-        if(this.gorcakic >= 2){
+        if (this.gorcakic >= 2) {
             this.bazmanal();
         }
     }
